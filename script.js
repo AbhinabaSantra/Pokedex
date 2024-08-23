803a025aec21bbd4d1d6d97a3dc41ece42abb24e
AbhinabaSantra <abhinabasantra84@gmail.com>
2024-08-23 15:07:36 +0530

    updated the serach bar for searching both names and pokedex numbers

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ import {pokedex} from "./data/data.js";
 let wrapperInner = '';
 //updating the webpage using DOM
 pokedex.forEach((pokemon)=>{
-    wrapperInner+=`
-        <div class="pokemon" data-name=${pokemon.name}>
+    wrapperInner+=`<div class="pokedata">
+        <div class="pokemon" data-name=${pokemon.name} data-pokedexNumber='${pokemon.pokedexNumber}'>
             <img src="images/Pokemon-Dataset/${pokemon.name}.png">
             <span class="name">${pokemon.name}</span>
             <div class="type">
@@ -15,11 +15,22 @@ pokedex.forEach((pokemon)=>{
                 <span class="pokedex-number">${pokemon.pokedexNumber}</span>
             </div>
         </div>
+        </div>
     `;
 })
 document.querySelector('.wrapper').innerHTML=wrapperInner;
 //Creating the search bar
 document.querySelector('.searchbar')
-    .addEventListener('keydown',(event)=>{
-        console.log(event.key);
-    })
+    .addEventListener('keyup',()=>{
+        let x=document.querySelector('.searchbar').value.toLowerCase();
+        console.log(document.querySelector('.pokemon').dataset);
+        document.querySelectorAll('.pokedata').forEach((pokemon)=>{
+            if(pokemon.querySelector('.pokemon').dataset.name.toLowerCase().includes(x) ||
+                pokemon.querySelector('.pokemon').dataset.pokedexnumber.includes(x))
+            {
+                pokemon.style.display = "block";
+            }else{
+                pokemon.style.display = "none";
+            }
+        })
+    });
