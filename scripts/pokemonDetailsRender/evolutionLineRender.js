5b437651823e31b679abdfedc205000251b7ce59
AbhinabaSantra <abhinabasantra84@gmail.com>
2024-08-25 23:42:42 +0530

    introduced the branched evolution line model

--- scripts/pokemonDetailsRender/evolutionLineRender.js
+++ scripts/pokemonDetailsRender/evolutionLineRender.js
@@ -7,17 +7,24 @@ export function evolutionRender(){
     if(document.querySelector('.evolution')){
         const selectedPokedexNumber = Number(localStorage.getItem('pokemonNumber'));
         searchEvolutionLine(selectedPokedexNumber);
+        
     }
 }
 
 // Function for searching the evolution line array in evolution.js 
 function searchEvolutionLine(selectedPokedexNumber){
-    evolution.forEach((evolutionLine)=>{
-        if(evolutionLine.includes(selectedPokedexNumber)){
-           generateEvolutionHTML(evolutionLine); 
-           evolutionStyling(evolutionLine,selectedPokedexNumber)
-        }
-    })
+    if([133,134,135,136].includes(selectedPokedexNumber)){
+        generateBrachedEvolution([133,134,135,136]);
+        evolutionStyling([133,134,135,136],selectedPokedexNumber);
+    }
+    else{
+        evolution.forEach((evolutionLine)=>{
+            if(evolutionLine.includes(selectedPokedexNumber)){
+            generateEvolutionHTML(evolutionLine); 
+            evolutionStyling(evolutionLine,selectedPokedexNumber);
+            }
+        })
+    }
 }
 
 // For searching the active Pokemon
@@ -59,4 +66,43 @@ function evolutionStyling(evolutionLine,activePokemonNumber){
             document.querySelector(`.stage${index+1}`).style.filter = 'grayscale(100%)'
         }
     });
+}
+
+// code for Branched Evolution of Eevee
+function generateBrachedEvolution(evolutionLine){
+    let evolutionHTML = '<div class="branchedEvo">'
+    evolutionLine.forEach((stageNumber,index)=>{
+        const pokemonStage = searchPokemon(stageNumber);
+        if(index == 0){
+            evolutionHTML += 
+                `
+                    <div class= "main">
+                        <div class="inner">
+                            <div class="stage stage${index+1}">
+                                <img class="img-pokeEvo" src="images/Pokemon-Dataset/${pokemonStage.name}.png" alt=${pokemonStage.name}>
+                            </div>
+                            <p> ${pokemonStage.name} </p>
+                        </div>
+                        <i class="fa-solid fa-chevron-right forward-arrow "></i>
+                    </div>
+                    <div class= "branched">
+                    `;
+        }
+        else{
+            evolutionHTML += 
+                `   
+                    
+                        <div class="inner">
+                            <div class="stage stage${index+1}">
+                                <img class="img-pokeEvo" src="images/Pokemon-Dataset/${pokemonStage.name}.png" alt=${pokemonStage.name}>
+                            </div>
+                            <p> ${pokemonStage.name} </p>
+                        </div>
+                    
+                `
+        }
+        
+    }); 
+    evolutionHTML += `</div></div>`
+    document.querySelector('.evolution').innerHTML = evolutionHTML;                 
 }
