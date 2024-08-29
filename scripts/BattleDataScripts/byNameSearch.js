e64287a485bc10f25c072d70a2bc1d6e02f76800
AbhinabaSantra <abhinabasantra84@gmail.com>
2024-08-29 17:57:14 +0530

    added the immunities tab

--- scripts/BattleDataScripts/byNameSearch.js
+++ scripts/BattleDataScripts/byNameSearch.js
@@ -2,26 +2,46 @@ import { pokemonDetails } from "../../data/pokemon-details.js";
 import { typeChart } from "../../data/typeAdvantage.js";
 import { searchColor } from "./colorSearch.js";
 
+let sectionHTML="";
+let weak=[];
+let immunity = []
 const byNameInput=document.querySelector('.pokeName') ;
 export function onclickByNameButton(){
     document.querySelector('.byNameButton').
         addEventListener('click',()=>{
+            sectionHTML="";
+            weak=[];
+            immunity = [];
+            HTMLRenderBattleDataSection();
             const pokemonName = byNameInput.value;
             searchPokemonName(pokemonName);
-            
+            HTMLRenderBattleDataSection();
+            colorBasedOnType();
             
         }); 
+    byNameInput.addEventListener('keypress',(event)=>{
+        if(event.key==='Enter'){
+            sectionHTML="";
+            weak=[];
+            immunity = [];
+            HTMLRenderBattleDataSection();
+            const pokemonName = byNameInput.value;
+            searchPokemonName(pokemonName);
+            HTMLRenderBattleDataSection();
+            colorBasedOnType();
+            byNameInput.value=""
+        }
+    })
 }
 
-function searchPokemonName(pokemonName){
-    let inputPokemon;
-    pokemonDetails.forEach((pokemon)=>{
-        if(pokemon.name.toLowerCase() === pokemonName.toLowerCase()){
-            inputPokemon = pokemon;
-        }
-    });
+function searchPokemonName(pokemonName) {
+    const inputPokemon = pokemonDetails.find((pokemon) =>
+        pokemon.name.toLowerCase() === pokemonName.toLowerCase()
+    );
     weakness(inputPokemon.type1);
+    immunities(inputPokemon.type1);
 }
+
 function searchType(type){
     
     for(let i = 0;i<typeChart.length;i++){
@@ -31,18 +51,17 @@ function searchType(type){
     }
 }
 function weakness(type){
-    let weak = []
     const typeAdvDetails = searchType(type);
     Object.entries(typeAdvDetails).forEach(([key,value])=>{
         if(value===0.5){
             weak.push(key.slice(8))
         }
     })
-    DOMRender(weak);
+    DOMHTMLWeakness(weak);
 }
 
-function DOMRender(weak){
-    let sectionHTML = `
+function DOMHTMLWeakness(weak){
+    sectionHTML += `
         <div class="weakness">
             <p class="heading"> WEAKNESS </p>
             <div class = "weak-type">
@@ -56,12 +75,42 @@ function DOMRender(weak){
             </div>
         </div>
     `;
-    document.querySelector('section').innerHTML = sectionHTML;
-    colorBasedOnType(weak)
 }
 
-function colorBasedOnType(weak){
-    weak.forEach((typeWeakness)=>{
-        document.querySelector(`.${typeWeakness}`).style.background = searchColor(typeWeakness);
+function colorBasedOnType(){
+    const types=weak.concat(immunity);
+    types.forEach((type)=>{
+        document.querySelector(`.${type}`).style.background = searchColor(type);
     })
+}
+function immunities(type){
+    const typeAdvDetails = searchType(type);
+    Object.entries(typeAdvDetails).forEach(([key,value])=>{
+        if(value===0){
+            immunity.push(key.slice(8))
+        }
+    })
+    if(immunity.length !== 0){
+        DOMHTMLImmunity(immunity);
+    }
+    console.log(immunity);
+}
+function DOMHTMLImmunity(immunity){
+    sectionHTML += `
+        <div class="immunity">
+            <p class="heading"> IMMUNITIES </p>
+            <div class = "immune-type">
+    `;
+    immunity.forEach((typeImmune)=>{
+        sectionHTML += `    
+            <p class="${typeImmune}"> ${typeImmune} </p>
+        `;
+    })
+    sectionHTML += `
+            </div>
+        </div>
+    `;
+}
+function HTMLRenderBattleDataSection(){
+    document.querySelector('section').innerHTML = sectionHTML || "<p class='error'>Pokemon not found. Please check the spelling</p>"        
 }
